b30af57abafe9c0a0f4c26b37e6ed4396acebff5
Steven Schmid <steschmid@gmx.ch>
2014-11-09 12:53:53 +0100

    update dem dotfiles for boxen compatibility

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,19 +1,11 @@
 # general
 alias ls='ls -Gla'
 alias ll='ls -hl'
-
 function ff { find . -iname "*$1*" }
-
 alias fh='history | grep'
 
 # ruby and development
 alias rc='rails console'
 alias qq='touch tmp/restart.txt'
 alias b='bundle'
-alias r='rspec'
-alias gg='git up'
-alias ql='powder link'
-alias vim='mvim -v --servername VIM'
-
-# vim
 alias vim='mvim -v --servername VIM'
