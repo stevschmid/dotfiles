50d85ae3fa7e4563b02d589d8321aa979818fdb7
Steven Schmid <stevschmid@gmail.com>
2015-07-16 00:30:18 +0200

    rbenv speedup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,11 @@
 # search for file name
 function ff { ag -g $1 }
+
+# speed up rbenv init by adding --no-rehash
+function rbenv {
+  if [ "$1" = 'init' ]
+  then
+    set -- $* --no-rehash
+  fi
+  command rbenv $*
+}
