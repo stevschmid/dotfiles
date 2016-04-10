321349a2edd93f8ee5c61e4235f9dd71a3bf953b
Steven Schmid <stevschmid@gmail.com>
2016-04-10 15:18:55 +0200

    Fix CTRL-H with Neovim

--- system/env.zsh
+++ system/env.zsh
@@ -1,3 +1,5 @@
 export TERM=screen-256color
 export LC_ALL=en_US.UTF-8
 export DISABLE_SPRING=1 # SPRING – NOT EVEN ONCE
+
+export TERMINFO="$HOME/.terminfo" # https://github.com/neovim/neovim/issues/2048
