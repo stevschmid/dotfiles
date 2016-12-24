542b1006b9ab1e8ad0790fbda5221d3ed304489f
Steven Schmid <stevschmid@gmail.com>
2016-12-24 12:01:32 +0100

    ツ

--- system/env.zsh
+++ system/env.zsh
@@ -3,3 +3,4 @@ export LC_ALL=en_US.UTF-8
 export DISABLE_SPRING=1 # SPRING – NOT EVEN ONCE
 export TERMINFO="$HOME/.terminfo" # https://github.com/neovim/neovim/issues/2048
 export SOCKET_DIR=/tmp/
+export HOMEBREW_NO_ANALYTICS=1
