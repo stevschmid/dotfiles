4fa450a2295193ffe27758f30f877ba5ba587793
Steven Schmid <stevschmid@gmail.com>
2018-01-09 09:31:22 +0100

    Erlang history

--- system/env.zsh
+++ system/env.zsh
@@ -6,3 +6,7 @@ export HOMEBREW_NO_ANALYTICS=1
 # https://github.com/neovim/neovim/issues/2048
 # does not work anymore, wait for new build (latest comments)
 # export TERMINFO="$HOME/.terminfo" 
+
+# Enable Erlang20+ history
+export ERL_AFLAGS="-kernel shell_history enabled"
+touch ~/.iex_history
