ac15adae51104d9f47954aa97bf9bd7b81bdb31b
Steven Schmid <stevschmid@gmail.com>
2024-06-12 15:37:14 +0200

    Update

--- system/env.zsh
+++ system/env.zsh
@@ -4,8 +4,15 @@ export SOCKET_DIR=/tmp/
 export HOMEBREW_NO_ANALYTICS=1
 # https://github.com/neovim/neovim/issues/2048
 # does not work anymore, wait for new build (latest comments)
-# export TERMINFO="$HOME/.terminfo" 
+# export TERMINFO="$HOME/.terminfo"
 
 # Enable Erlang20+ history
 export ERL_AFLAGS="-kernel shell_history enabled"
 touch ~/.iex_history
+
+# https://github.com/rails/rails/issues/38560
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
+# https://github.com/basecamp/solid_queue/issues/145
+# https://github.com/ged/ruby-pg/issues/538#issuecomment-1591629049
+export PGGSSENCMODE="disable"
